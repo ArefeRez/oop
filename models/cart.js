2c6534eceeb6ebb6e569df13daecf9b0a3f2dc8a
Arefe <arefe2003.rezaee@gmail.com>
2024-11-09 14:34:18 +0330

    last part

--- models/cart.js
+++ models/cart.js
@@ -1,66 +1,104 @@
-class Cart{
-    constructor(parent, price,products){
-        this.parent=parent;
-        this.price=price;
-        this.products=[];
-        this.toShow=[];
+class Cart {
+    constructor(parent, price) {
+      this.parent = parent;
+      this.price = price;
+      this.products = [];
+      this.toShow = [];
+      this.parent.addEventListener("click", this);
     }
-    showProducts(){
-        this.toShow=[...new Set(this.products)];
-        this.parent.innerHTML="";
-        this.toShow.forEach(product=>{
-            const qty=this.products.filter(p=>p.id===product.id).length;
-            this.createdCard(product,qty);
-        })
+    showProducts() {
+      this.toShow = [...new Set(this.products)];
+      this.parent.innerHTML = "";
+      this.toShow.forEach((product) => {
+        const qty = this.products.filter((p) => p.id=== product.id).length;
+        this.createdCard(product, qty);
+      });
+      this.calculateTotalPrice();
     }
-    createdCard(data,qty){
-        // console.log(data);
-        const cardEle=document.createElement("div");
-        const imgEle=this.productImg(data);
-        const infoEle=this.productInfo(data);
-        const controlEle=this.productControl(data,qty);
-
-
-
-        cardEle.innerHTML=imgEle;
-        cardEle.innerHTML+=infoEle;
-        cardEle.innerHTML+=controlEle;
-        this.parent.appendChild(cardEle);
-
+    createdCard(data, qty) {
+      //console.log(data);
+      const cardEle = document.createElement("div");
+  
+      const imgEle = this.productImg(data);
+      const infoEle = this.productInfo(data);
+      const controlEle = this.productControl(data, qty);
+  
+      cardEle.innerHTML = imgEle;
+      cardEle.innerHTML += infoEle;
+      cardEle.innerHTML += controlEle;
+      this.parent.appendChild(cardEle);
     }
-    productImg(data){
-        const {image,alt}=data;
-        const imgJSX=`<img alt=${alt} src=${image}>`;
-        return imgJSX;
+    productImg(data) {
+      const { image, alt } = data;
+      const imgJSX = `<img alt=${alt} src=${image}>`;
+      return imgJSX;
     }
-    productInfo(data){
-        const {name, price}=data;
-        const infoJSX=`
-        <div id="cart-info>
-        <h4>${name}</h4>
-        <p>${price}</p>
-        </div>
-        `;
-        return infoJSX;
+    productInfo(data) {
+      const { name, price } = data;
+      const infoJSX = `
+          <div id="cart-info">
+              <h4>${name}</h4>
+              <p>${price}</p>
+          </div>
+      `;
+      return infoJSX;
     }
-    productControl(data,qty){
-        const {id}=data;
-        const controlJSX=`
-        <div id="cart-control>
-            <div>
-            <button data-id=${id}>-</button>
-            <span>${qty}</span>
-            <button data-id=${id}>+</button>
-            </div>
-            <button data-id=${id}>Remove</button>
-        </div>
-        `;
-        return controlJSX;
+    productControl(data, qty) {
+      const { id } = data;
+      const controlJSX = `
+          <div id="cart-control">
+              <div>
+              <button data-id=${id}>-</button>
+              <span>${qty}</span>
+              <button  data-id=${id}>+</button>
+              </div>
+              <button  data-id=${id}>Remove</button>
+          </div>
+      `;
+      return controlJSX;
     }
-}
-
-
-
-
-
-export default Cart;
+    handleEvent(event) {
+      //console.log("click")
+      const tagName = event.target.tagName;
+      const id = event.target.dataset.id;
+      const type = event.target.innerText;
+  
+      if (tagName !== "BUTTON") return;
+      //console.log("ujhuj");
+      switch (type) {
+        case "+":
+          this.increase(id);
+          break;
+  
+        case "-":
+          this.decrease(id);
+          break;
+  
+        case "Remove":
+          this.remove(id);
+          break;
+      }
+    }
+  
+    increase(id) {
+      const product = this.products.find((p) => p.id === +id);
+      this.products.push(product);
+      this.showProducts();
+    }
+    decrease(id) {
+      const index = this.products.findIndex((p) => p.id === id);
+      this.products.splice(index, 1);
+      this.showProducts();
+    }
+    remove(id) {
+      const newProducts = this.products.filter((p) => p.id !== +id);
+      this.products = newProducts;
+      this.showProducts();
+    }
+    calculateTotalPrice() {
+      const total = this.products.reduce((acc, cur) => (acc += cur.price), 0);
+      this.price.innerText = "$ " + total;
+    }
+  }
+  
+  export default Cart;
